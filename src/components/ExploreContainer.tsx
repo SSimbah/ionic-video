340be3b5e0767bb6df3fe446614e309d18806de4
Kyle Fernandez <fkyleowie@outlook.com>
2022-04-07 15:33:33 +0800

    ionic react fullscreen video

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -5,8 +5,9 @@ interface ContainerProps { }
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <video width="320" height="240" controls>
+        <source src='./video/samplevid.mp4' type="video/mp4" />
+      </video>
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,17 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { StatusBar } from '@capacitor/status-bar';
 import './Home.css';
 
+
 const Home: React.FC = () => {
+  
+  StatusBar.setOverlaysWebView({overlay: true});  
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <video id='videoContainer' autoPlay loop muted poster='/assets/video/poster.png'>
+          <source src="/assets/video/samplevid.mp4" type="video/mp4"></source>
+        </video>
       </IonContent>
     </IonPage>
   );
